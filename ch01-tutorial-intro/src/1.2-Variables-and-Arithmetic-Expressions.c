13c7301488add8c9c25b610065c852b4c881bfaf
FlaredRoverCodes <flaredrovercodes@gmail.com>
2026-08-29 22:18:39 +0800

    floating-point arithmetic

--- ch01-tutorial-intro/src/1.2-Variables-and-Arithmetic-Expressions.c
+++ ch01-tutorial-intro/src/1.2-Variables-and-Arithmetic-Expressions.c
@@ -2,13 +2,18 @@
 
 int main()
 {
-    int fahr, celsius, lower = 0, upper = 300, step = 20;
+    float fahr, celsius;
+    int lower, upper, step;
+
+    lower = 0;
+    upper = 300;
+    step = 20;
 
     fahr = lower;
     while(fahr <= upper)
     {
-        celsius = 5 * (fahr - 32) / 9;
-        printf("%3d\t%6d\n", fahr, celsius);
+        celsius = (float)5 * (fahr - 32) / 9;
+        printf("%3d\t%6.1f\n", fahr, celsius);
         fahr += step;
     }
 }
